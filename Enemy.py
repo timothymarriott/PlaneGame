07a84297ce3b9f2789022b4c4d666a5589d78ad0
Sebastian Strano <thebeastlegendgaming2234561@gmail.com>
2024-05-16 13:04:37 +0800

    made enemy not as op

--- Enemy.py
+++ Enemy.py
@@ -30,7 +30,7 @@ class Enemy:
         self.distanceToPlayerY = abs(Window.WINDOW._game._player.posY - self.posY)
         self.distanceToPlayer = sqrt(self.distanceToPlayerX * self.distanceToPlayerX + self.distanceToPlayerY * self.distanceToPlayerY)
    
-        if self.distanceToPlayer < 150:
+        if self.distanceToPlayer < 100:
             if self.posY < Window.WINDOW._game._player.posY:
                 if Window.WINDOW._game._player.doRender == True:
                     if self.shootTimer > 1:
