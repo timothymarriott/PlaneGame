6cbf73ae95a139ce4d79a546ea98be269f812beb
Sebastian Strano <thebeastlegendgaming2234561@gmail.com>
2024-05-17 14:13:59 +0800

    player collides with enemy and enemies die when going offscreen

--- Enemy.py
+++ Enemy.py
@@ -4,6 +4,7 @@ from SpriteRegistry import *
 import Window
 from math import sqrt
 from EnemyBullet import EnemyBullet
+from Explosion import  Explosion
 
 class Enemy:
     posX = 0
@@ -14,6 +15,7 @@ class Enemy:
     hasShot = False
     shootTimer = 0
     movementTimer = 0
+    enemyAliveTimer = 0
 
     def __init__(self, x: int, y: int) -> None:
         self.posX = x
@@ -22,6 +24,7 @@ class Enemy:
 
     def draw(self, deltaTime: float, time: float):
 
+        self.enemyAliveTimer += deltaTime
 
         if self.hasShot == True:
             self.movementTimer += deltaTime
@@ -41,7 +44,7 @@ class Enemy:
         self.distanceToPlayerX = abs(Window.WINDOW._game._player.posX - self.posX)
         self.distanceToPlayerY = abs(Window.WINDOW._game._player.posY - self.posY)
         self.distanceToPlayer = sqrt(self.distanceToPlayerX * self.distanceToPlayerX + self.distanceToPlayerY * self.distanceToPlayerY)
-   
+
         if self.distanceToPlayer < 100:
             if self.posY < Window.WINDOW._game._player.posY:
                 if Window.WINDOW._game._player.doRender == True:
@@ -67,4 +70,19 @@ class Enemy:
                             self.hasShot= True
                             self.shootTimer = 0
         
+        if self.enemyAliveTimer  > 5:
+            if self.posY > Window.WINDOW._actualHeight and self in Window.WINDOW._game._enemies :
+                Window.WINDOW._game._enemies.remove(self)
+
+           
+            if self.posY < 10 and self in Window.WINDOW._game._enemies:
+                Window.WINDOW._game._enemies.remove(self)
+
+        if self.distanceToPlayer < 20:
+            if Window.WINDOW._game._player.doRender != False:
+                Window.WINDOW._game._player.doRender = False
+                Window.WINDOW._game._explosions.append(Explosion(Window.WINDOW._game._player.posX, Window.WINDOW._game._player.posY))
+                Window.WINDOW._game._explosions.append(Explosion(self.posX, self.posY))
+                Window.WINDOW._game._enemies.remove(self)
+
         pass
