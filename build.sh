2e145171dd2ece7b6a3aac2bbfefff0ed9145dc5
Timothy Marriott <timothyrmarriott@gmail.com>
2024-05-30 09:04:12 +0800

    Moving onto boss movement, pun intended

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #bin/bash
+rm -r ./dist/1942.app/
 pyinstaller --windowed --onedir -n 1942 -i ./icon-windowed.icns -F ./PlaneGame.py
 cp -R ./assets ./dist/1942.app/Contents/Resources/assets/
 rm -R ./build
+rm ./dist/1942.app/
 rm ./dist/1942
 cd ./dist
-zip -r 1942-Build-$(date +%Y%m%d_%H%M%S%Z) 1942.app
+zip -r 1942-Build-$(date +%d-%M-%Y) 1942.app
+cd ..
 open ./dist
